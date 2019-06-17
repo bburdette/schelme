0ddabae89416c91dd5f0fb8781013e8291240010
Ben Burdette <bburdette@gmail.com>
2019-06-17 09:37:25 -0600

    code formatting

--- examples/bots/server/src/main.rs
+++ examples/bots/server/src/main.rs
@@ -1,26 +1,27 @@
+extern crate actix;
+extern crate actix_web;
 extern crate crypto_hash;
+extern crate env_logger;
 extern crate failure;
+extern crate futures;
 extern crate lettre;
 extern crate lettre_email;
+extern crate openssl;
 extern crate rand;
 extern crate serde_json;
 extern crate time;
-extern crate uuid;
-extern crate actix_web;
-extern crate futures;
 extern crate toml;
-extern crate openssl;
-extern crate actix;
-extern crate env_logger;
+extern crate uuid;
 #[macro_use]
 extern crate log;
 
-use actix_web::{AsyncResponder, server, App, Result, HttpMessage, HttpRequest, Responder,
-                HttpResponse, http};
-use actix_web::middleware::Logger;
 use actix_web::fs::NamedFile;
-use actix_web::http::{Method,StatusCode};
+use actix_web::http::{Method, StatusCode};
+use actix_web::middleware::Logger;
 use actix_web::Binary;
+use actix_web::{
+  http, server, App, AsyncResponder, HttpMessage, HttpRequest, HttpResponse, Responder, Result,
+};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[macro_use]
@@ -30,15 +31,13 @@ use failure::Error;
 use futures::future::Future;
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
-mod util;
 mod process_json;
-use process_json::{PublicMessage, 
-                   process_public_json, ServerResponse};
-
+mod util;
+use process_json::{process_public_json, PublicMessage, ServerResponse};
 
 fn files(req: &HttpRequest) -> Result<NamedFile> {
   let path: PathBuf = req.match_info().query("tail")?;
-  info!("files: {:?}", path); 
+  info!("files: {:?}", path);
   let stpath = Path::new("static/").join(path);
   Ok(NamedFile::open(stpath)?)
 }
@@ -53,47 +52,52 @@ fn sitemap(req: &HttpRequest) -> Result<NamedFile> {
   Ok(NamedFile::open(stpath)?)
 }
 
-
 /// simple index handler
 fn mainpage(req: &HttpRequest) -> Result<HttpResponse> {
-    info!("remote ip: {:?}, request:{:?}", req.connection_info().remote(), req);
+  info!(
+    "remote ip: {:?}, request:{:?}",
+    req.connection_info().remote(),
+    req
+  );
 
-    match util::load_string("static/index.html") { 
-      Ok(s) => {
-        // response
-        Ok(HttpResponse::build(StatusCode::OK)
-            .content_type("text/html; charset=utf-8")
-            .body(s))
-      }
-      Err(e) => Err(e.into())
+  match util::load_string("static/index.html") {
+    Ok(s) => {
+      // response
+      Ok(
+        HttpResponse::build(StatusCode::OK)
+          .content_type("text/html; charset=utf-8")
+          .body(s),
+      )
     }
+    Err(e) => Err(e.into()),
+  }
 }
 
 fn public(req: &HttpRequest) -> Box<Future<Item = String, Error = Error>> {
-  let ci = req.connection_info().clone(); 
+  let ci = req.connection_info().clone();
   req
     .json()
     .from_err()
     .and_then(move |msg: PublicMessage| {
-      Ok(match process_json::process_public_json(&(ci.remote()), msg) {
-        Ok(sr) => {
-          match serde_json::to_string(&sr) {
+      Ok(
+        match process_json::process_public_json(&(ci.remote()), msg) {
+          Ok(sr) => match serde_json::to_string(&sr) {
             Ok(s) => s,
             Err(e) => e.to_string(),
+          },
+          Err(e) => {
+            error!("uh oh, 'public' err: {:?}", e);
+            let se = ServerResponse {
+              what: "server error".to_string(),
+              content: serde_json::Value::String(e.to_string()),
+            };
+            match serde_json::to_string(&se) {
+              Ok(s) => s,
+              Err(e) => e.to_string(),
+            }
           }
-        }
-        Err(e) => {
-          error!("uh oh, 'public' err: {:?}", e);
-          let se = ServerResponse {
-            what: "server error".to_string(),
-            content: serde_json::Value::String(e.to_string()),
-          };
-          match serde_json::to_string(&se) {
-            Ok(s) => s,
-            Err(e) => e.to_string(),
-          }
-        }
-      })
+        },
+      )
     })
     .responder()
 }
@@ -123,26 +127,21 @@ fn defcon() -> Config {
   }
 }
 
-
 fn load_config() -> Config {
   match util::load_string("config.toml") {
     Err(e) => {
       error!("error loading config.toml: {:?}", e);
       defcon()
     }
-    Ok(config_str) => {
-      match toml::from_str(config_str.as_str()) {
-        Ok(c) => c,
-        Err(e) => {
-          error!("error loading config.toml: {:?}", e);
-          defcon()
-        }
+    Ok(config_str) => match toml::from_str(config_str.as_str()) {
+      Ok(c) => c,
+      Err(e) => {
+        error!("error loading config.toml: {:?}", e);
+        defcon()
       }
-    }
+    },
   }
 }
-
-
 
 fn main() {
   let config = load_config();
@@ -168,45 +167,45 @@ fn main() {
 
   {
     let s = server::new(move || {
-      
-        App::new()
-            .resource("/public", |r| r.method(Method::POST).f(public))
-            .resource(r"/static/{tail:.*}", |r| r.method(Method::GET).f(files))
-            .resource("/favicon.ico", |r| r.method(Method::GET).f(favicon))
-            .resource("/sitemap.txt", |r| r.method(Method::GET).f(sitemap))
-            .resource(r"/{tail:.*}", |r| r.method(Method::GET).f(mainpage))
-
+      App::new()
+        .resource("/public", |r| r.method(Method::POST).f(public))
+        .resource(r"/static/{tail:.*}", |r| r.method(Method::GET).f(files))
+        .resource("/favicon.ico", |r| r.method(Method::GET).f(favicon))
+        .resource("/sitemap.txt", |r| r.method(Method::GET).f(sitemap))
+        .resource(r"/{tail:.*}", |r| r.method(Method::GET).f(mainpage))
     });
- 
+
     match optbuilder {
       Some(builder) => s.bind_ssl(format!("{}:{}", config.ip, config.port), builder),
       None => s.bind(format!("{}:{}", config.ip, config.port)),
     }
-  }.expect(format!("Can not bind to port {}", config.port).as_str())
-    .start();
+  }
+  .expect(format!("Can not bind to port {}", config.port).as_str())
+  .start();
 
   // do some http redirecting?
   let _ = match (config.redirectport, config.redirectdomain) {
-    (Some(port), Some(domain)) => {
-      Some(
-        server::new(move || {
-          App::with_state(domain.clone())
-            .resource("{all:.*}", |r| {
-              r.f(|r| {
-                info!("redirect!{}", r.path());
-                HttpResponse::Found()
-                  .header(http::header::LOCATION, format!("{}{}?{}", r.state(), r.path(), r.query_string()))
-                  .finish()
-              })
+    (Some(port), Some(domain)) => Some(
+      server::new(move || {
+        App::with_state(domain.clone())
+          .resource("{all:.*}", |r| {
+            r.f(|r| {
+              info!("redirect!{}", r.path());
+              HttpResponse::Found()
+                .header(
+                  http::header::LOCATION,
+                  format!("{}{}?{}", r.state(), r.path(), r.query_string()),
+                )
+                .finish()
             })
-            .middleware(Logger::default())
-            .middleware(Logger::new("REDIRECTOR: %a %{User-Agent}i"))
-
-        }).bind(format!("{}:{}", config.ip, port))
-          .expect(format!("Can not bind to port {}", port).as_str())
-          .start(),
-      )
-    }
+          })
+          .middleware(Logger::default())
+          .middleware(Logger::new("REDIRECTOR: %a %{User-Agent}i"))
+      })
+      .bind(format!("{}:{}", config.ip, port))
+      .expect(format!("Can not bind to port {}", port).as_str())
+      .start(),
+    ),
     _ => None,
   };
 
--- examples/bots/server/src/process_json.rs
+++ examples/bots/server/src/process_json.rs
@@ -1,10 +1,10 @@
 extern crate serde_json;
-use serde_json::Value;
-use failure::Error;
-use util;
 use failure;
+use failure::Error;
+use serde_json::Value;
 use std::fs::File;
 use std::io::Read;
+use util;
 
 #[derive(Deserialize, Debug)]
 pub struct PublicMessage {
@@ -26,46 +26,47 @@ pub struct ServerResponse {
   pub content: Value,
 }
 
-
 fn load_script(name: &str) -> Result<ServerResponse, Error> {
-  Ok(ServerResponse { 
-      what: "script".to_string(),
-      content: serde_json::from_str(util::load_string(format!("scripts/{}", name).as_str())?.as_str())?
-    })
+  Ok(ServerResponse {
+    what: "script".to_string(),
+    content: serde_json::from_str(
+      util::load_string(format!("scripts/{}", name).as_str())?.as_str(),
+    )?,
+  })
 }
 
+fn save_script(name: &str, data: &Value) -> Result<ServerResponse, Error> {
+  util::write_string(
+    format!("scripts/{}", name).as_str(),
+    data.to_string().as_str(),
+  )?;
 
-
-fn save_script(name: &str, data: &Value) -> Result<ServerResponse, Error> { 
-  util::write_string(format!("scripts/{}", name).as_str(), data.to_string().as_str())?;
-
-  Ok(ServerResponse { 
-      what: "script written!".to_string(),
-      content: serde_json::Value::Null,
-    })
+  Ok(ServerResponse {
+    what: "script written!".to_string(),
+    content: serde_json::Value::Null,
+  })
 }
 
 // public json msgs don't require login.
-pub fn process_public_json( ip: &Option<&str>, msg: PublicMessage) -> Result<Option<ServerResponse>, Error> {
+pub fn process_public_json(
+  ip: &Option<&str>,
+  msg: PublicMessage,
+) -> Result<Option<ServerResponse>, Error> {
   match msg.what.as_str() {
-    "getscript" => {
-      match msg.data {
-        None => Ok(Some(ServerResponse {
-          what: "no script specified!".to_string(),
-          content: serde_json::Value::Null,
-        })),
-        Some(data) => {
-          info!("public getscript:{}", data);
-          match data.to_string().as_str() {
-            name  => (load_script(name)).map(Some),
-          }
-          }
+    "getscript" => match msg.data {
+      None => Ok(Some(ServerResponse {
+        what: "no script specified!".to_string(),
+        content: serde_json::Value::Null,
+      })),
+      Some(data) => {
+        info!("public getscript:{}", data);
+        match data.to_string().as_str() {
+          name => (load_script(name)).map(Some),
         }
       }
+    },
 
     // get Meta Tag Base = getmtb
     wat => Err(failure::err_msg(format!("invalid 'what' code:'{}'", wat))),
   }
 }
-
-
